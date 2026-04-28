c25965b74c02e4c8db8f4bcbc22f5e1d1116b4af
Kali-Kana <praneetchilla2@gmail.com>
2026-04-27 20:28:40 -0400

    rename StudySyncTests.java to UnitTests.java

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StudySyncTests {
+public class UnitTests {
 
     private static final String ENV_FILE = ".env";
 
